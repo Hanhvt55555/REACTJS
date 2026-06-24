8926a0be5462a9aa608bd627ebd788920f7f30ec
Hanhvt55555 <hanhvt@rikkeisoft.com>
2026-06-24 11:59:29 +0900

    crud vs redux - CREATE

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,6 +1,8 @@
 import actionTypes from "./actionTypes";
-import { getAllCodeService } from "../../services/userService";
-import { dispatch } from "../../redux";
+import {
+  getAllCodeService,
+  createNewUserService,
+} from "../../services/userService";
 
 // export const fetchGenderStart = () => ({
 //   type: actionTypes.FETCH_GENDER_START,
@@ -9,6 +11,9 @@ import { dispatch } from "../../redux";
 export const fetchGenderStart = () => {
   return async (dispatch, getState) => {
     try {
+      dispatch({
+        type: actionTypes.FETCH_GENDER_START,
+      });
       let res = await getAllCodeService("GENDER");
       if (res && res.errCode === 0) {
         dispatch(fetchGenderSuccess(res.data));
@@ -17,7 +22,7 @@ export const fetchGenderStart = () => {
       }
     } catch (e) {
       dispatch(fetchGenderFailed());
-      console.log("fetchGenderStart", e);
+      console.log("fetchGenderFailed", e);
     }
   };
 };
@@ -29,4 +34,79 @@ export const fetchGenderSuccess = (genderData) => ({
 
 export const fetchGenderFailed = () => ({
   type: actionTypes.FETCH_GENDER_FAILED,
+});
+
+export const fetchPositionSuccess = (positionData) => ({
+  type: actionTypes.FETCH_POSITION_SUCCESS,
+  data: positionData,
+});
+
+export const fetchPositionFailed = () => ({
+  type: actionTypes.FETCH_POSITION_FAILED,
+});
+
+export const fetchRoleSuccess = (roleData) => ({
+  type: actionTypes.FETCH_ROLE_SUCCESS,
+  data: roleData,
+});
+
+export const fetchRoleFailed = () => ({
+  type: actionTypes.FETCH_ROLE_FAILED,
+});
+
+export const fetchPositionStart = () => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await getAllCodeService("POSITION");
+      if (res && res.errCode === 0) {
+        dispatch(fetchPositionSuccess(res.data));
+      } else {
+        dispatch(fetchPositionFailed());
+      }
+    } catch (e) {
+      dispatch(fetchPositionFailed());
+      console.log("fetchPositionFailed", e);
+    }
+  };
+};
+
+export const fetchRoleStart = () => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await getAllCodeService("ROLE");
+      if (res && res.errCode === 0) {
+        dispatch(fetchRoleSuccess(res.data));
+      } else {
+        dispatch(fetchRoleFailed());
+      }
+    } catch (e) {
+      dispatch(fetchRoleFailed());
+      console.log("fetchRoleFailed", e);
+    }
+  };
+};
+
+export const createNewUser = (data) => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await createNewUserService(data);
+      console.log("hoidanIT check create user redux: ", res);
+      if (res && res.errCode === 0) {
+        dispatch(saveUserSuccess());
+      } else {
+        dispatch(saveUserFailed());
+      }
+    } catch (e) {
+      dispatch(saveUserFailed());
+      console.log("saveUserFailed", e);
+    }
+  };
+};
+
+export const saveUserFailed = () => ({
+  type: "CREATE_USER_FAILED",
+});
+
+export const saveUserSuccess = () => ({
+  type: "CREATE_USER_SUCCESS",
 });
--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,6 +1,7 @@
 import actionTypes from "../actions/actionTypes";
 
 const initialState = {
+  isLoadingGender: false,
   genders: [],
   roles: [],
   positions: [],
@@ -8,20 +9,50 @@ const initialState = {
 
 const adminReducer = (state = initialState, action) => {
   switch (action.type) {
+    //gender
     case actionTypes.FETCH_GENDER_START:
-      console.log("hoidanit fire fetch gender start: ", action);
-      return {
-        ...state,
-      };
-    case actionTypes.FETCH_GENDER_SUCCESS:
       let copyState = { ...state };
-      copyState.genders = action.data;
-      console.log("hoidanit fire fetch gender success: ", copyState);
+      copyState.isLoadingGender = true;
+      console.log("hoidanit fire fetch gender start: ", action);
       return {
         ...copyState,
       };
+    case actionTypes.FETCH_GENDER_SUCCESS:
+      state.genders = action.data;
+      state.isLoadingGender = false;
+      console.log("hoidanit fire fetch gender success: ", action);
+      return {
+        ...state,
+      };
     case actionTypes.FETCH_GENDER_FAILED:
       console.log("hoidanit fire fetch gender fail: ", action);
+      state.isLoadingGender = false;
+      state.genders = [];
+      return {
+        ...state,
+      };
+
+    //position
+    case actionTypes.FETCH_POSITION_SUCCESS:
+      state.positions = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_POSITION_FAILED:
+      state.isLoadingPosition = false;
+      state.positions = [];
+      return {
+        ...state,
+      };
+
+    //Role
+    case actionTypes.FETCH_ROLE_SUCCESS:
+      state.roles = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_ROLE_FAILED:
+      state.roles = [];
       return {
         ...state,
       };
